19d66e00319920214a422f9e8b42ddd600edafa4
Robzon33 <48408667+Robzon33@users.noreply.github.com>
2020-08-07 08:50:38 +0200

    Update Platform.py
    Added Detection of Pi 4

--- Adafruit_GPIO/Platform.py
+++ Adafruit_GPIO/Platform.py
@@ -82,14 +82,16 @@ def pi_revision():
 
 
 def pi_version():
-    """Detect the version of the Raspberry Pi.  Returns either 1, 2 or
-    None depending on if it's a Raspberry Pi 1 (model A, B, A+, B+),
-    Raspberry Pi 2 (model B+), or not a Raspberry Pi.
+    """Detect the version of the Raspberry Pi.  Returns either 1, 2, 
+    3, 4 or none depending on if it's a Raspberry Pi 1 (model A, B, A+, B+),
+    Raspberry Pi 2 (model B+), Raspberry Pi 3, Raspberry Pi 4 or not a 
+    Raspberry Pi.
     """
     # Check /proc/cpuinfo for the Hardware field value.
     # 2708 is pi 1
     # 2709 is pi 2
     # 2835 is pi 3 on 4.9.x kernel
+    # 2711 is pi 4
     # Anything else is not a pi.
     with open('/proc/cpuinfo', 'r') as infile:
         cpuinfo = infile.read()
@@ -108,6 +110,9 @@ def pi_version():
     elif match.group(1) == 'BCM2835':
         # Pi 3 / Pi on 4.9.x kernel
         return 3
+    elif match.group(1) == 'BCM2711':
+        # Pi 4
+        return 4
     else:
         # Something else, not a pi.
         return None
